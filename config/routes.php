5da17a5df4c3f19ce858cf5b008b6829608ac2c4
sebdurrbach <sebastien.durrbach@gmail.com>
2019-10-09 17:06:49 +0200

    Mise en place du service manager

--- config/routes.php
+++ config/routes.php
@@ -6,25 +6,72 @@ use App\Database;
 use App\Model\Article as ArticleModel;
 use App\Model\Comment as CommentModel;
 
+class ServicesManager
+{
+    protected $services = [];
+
+    public function register(string $name, callable $fn)
+    {
+        $this->services[$name] = $fn;
+    }
+
+    public function get(string $name)
+    {
+        $constructor = $this->services[$name];
+        return $constructor($this);
+    }
+}
+
+$manager = new ServicesManager();
+
+$manager->register('controller.article', function (ServicesManager $manager) {
+    $articleModel = $manager->get('model.article');
+    $commentModel = $manager->get('model.comment');
+
+    return new Article($articleModel, $commentModel);
+});
+
+$manager->register('controller.comment', function (ServicesManager $manager) {
+    $articleModel = $manager->get('model.article');
+    $commentModel = $manager->get('model.comment');
+
+    return new Comment($commentModel, $articleModel);
+});
+
+$manager->register('model.article', function (ServicesManager $manager) {
+    $database = $manager->get('database');
+    return new ArticleModel($database);
+});
+
+$manager->register('model.comment', function (ServicesManager $manager) {
+    $database = $manager->get('database');
+    return new CommentModel($database);
+});
+
+$manager->register('database', function () {
+    return new Database();
+});
+
+
 return [
-    '/' => function () {
-        $controller = new Article(new ArticleModel(new Database), new CommentModel(new Database));
+    '/' => function () use ($manager) {
+        $controller = $manager->get('controller.article');
         $controller->index();
     },
-    '/article/{id}/delete' => function (array $params) {
-        $controller = new Article(new ArticleModel(new Database), new CommentModel(new Database));
+    '/article/{id}/delete' => function (array $params) use ($manager) {
+        $controller = $manager->get('controller.article');
         $controller->delete($params['id']);
     },
-    '/article/{id}/comment' => function (array $params) {
-        $controller = new Comment(new CommentModel(new Database), new ArticleModel(new Database));
+    '/article/{id}/comment' => function (array $params) use ($manager) {
+        $controller = $manager->get('controller.comment');
         $controller->create();
     },
-    '/article/{id}' => function (array $params) {
-        $controller = new Article(new ArticleModel(new Database), new CommentModel(new Database));
+    '/article/{id}' => function (array $params) use ($manager) {
+        $controller = $manager->get('controller.article');
         $controller->show((int) $params['id']);
     },
-    '/comment/{id}/delete' => function (array $params) {
-        $controller = new Comment(new CommentModel(new Database), new ArticleModel(new Database));
+    '/comment/{id}/delete' => function (array $params) use ($manager) {
+        $controller = $manager->get('controller.comment');
         $controller->delete($params['id']);
     }
 ];
